801a011171547c246c76f5e88c8eeb01fb570300
David Schindler <david.schindler97@gmail.com>
2021-04-29 01:11:08 +0200

    qr scan working

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import {QRScanner} from "@ionic-native/qr-scanner/ngx";
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+      QRScanner,
+      { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,67 @@
+import {Component} from '@angular/core';
+import {QRScanner, QRScannerStatus} from '@ionic-native/qr-scanner/ngx';
+
+
+@Component({
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+    private qrScan: any;
+
+    constructor(private qrScanner: QRScanner) {
+    }
+
+    timeLeft: number = 5;
+    interval;
+
+    startTimer() {
+        const ionApp = <HTMLElement>document.getElementsByTagName("ion-app")[0];
+        ionApp.style.display = "none";
+        this.interval = setInterval(() => {
+            if(this.timeLeft > 0) {
+                this.timeLeft--;
+            } else {
+                ionApp.style.display = "block";
+                this.timeLeft = 5;
+            }
+        },1000)
+    }
+
+    onScanClick() {
+        //this.startTimer();
+        this.qrScanner.prepare()
+            .then((status: QRScannerStatus) => {
+                if (status.authorized) {
+                    // camera permission was granted
+                    const ionApp = <HTMLElement>document.getElementsByTagName("ion-app")[0];
+                    ionApp.style.display = "none";
+                    this.qrScanner.show();
+
+
+                    // start scanning
+                    let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+                        console.log('Scanned something', text);
+                        ionApp.style.display = "block";
+                        this.qrScanner.hide(); // hide camera preview
+                        scanSub.unsubscribe(); // stop scanning
+                    });
+
+                } else if (status.denied) {
+                    // camera permission was permanently denied
+                    // you must use QRScanner.openSettings() method to guide the user to the settings page
+                    // then they can grant the permission from there
+                } else {
+                    // permission was denied, but not permanently. You can ask for permission again at a later time.
+                }
+            })
+            .catch((e: any) => console.log('Error is', e));
+    }
+
+    stopScanning() {
+        (window.document.querySelector('ion-app') as HTMLElement).classList.remove('cameraView');
+        this.qrScanner.hide();
+        this.qrScanner.destroy();
+    }
+}
